41808e57ee0049525e9c6b42a23155f7811a2913
Gim128 <gimhanpabasara4@gmail.com>
2022-02-01 00:01:42 +0530

    setup project & create folders

---
+++ src/containers/whatDREAMLABS/WhatDRL.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatDRL.css';
+
+const WhatDreamLabs = () => {
+  return (
+    <div>
+        WhatDreamLabs
+    </div>
+  );
+};
+
+export default WhatDreamLabs;
